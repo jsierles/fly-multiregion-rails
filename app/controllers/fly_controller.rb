8abc4eb2a959ca8eaf6b0651faf972a43883f064
Joshua Sierles <joshua@chatterbug.com>
2021-06-19 05:54:24 +0200

    Show the database host for reads and writes

--- app/controllers/fly_controller.rb
+++ app/controllers/fly_controller.rb
@@ -1,11 +1,16 @@
 class FlyController < ApplicationController
+  before_action :parse_db
+
   def show
-    db = URI.parse(ENV['DATABASE_URL'])
-    @db_host_port = "#{db.host}#{db.port}"
   end
 
   def write
     Writeable.create!(content: "written")
     render :show
+  end
+
+  def parse_db
+    db = URI.parse(ENV['DATABASE_URL'])
+    @db_host_port = "#{db.host}#{db.port}"
   end
 end
